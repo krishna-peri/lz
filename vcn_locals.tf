67b0cf0fb4a344379991fc4ee9c3a6649616b21e
krishna-peri <krishna.peri@centroid.com>
2024-09-20 12:19:39 -0400

    Create vcn_locals.tf

---
+++ vcn_locals.tf
@@ -0,0 +1,53 @@
+locals {
+  vcn_configs = [
+    {
+      name   = "iad-vcn-dmz"
+      cidr   = "135.136.129.0/26"
+      subnets = [
+        { name = "subnet1-1", cidr = "10.0.1.0/24" },
+        { name = "subnet1-2", cidr = "10.0.2.0/24" }
+      ]
+    },
+    {
+      name   = "iad-vcn-platform"
+      cidr   = "135.136.129.128/25"
+      subnets = [
+        { name = "subnet1-1", cidr = "10.0.1.0/24" },
+        { name = "subnet1-2", cidr = "10.0.2.0/24" }
+      ]
+    },
+    {
+      name   = "iad-vcn-pd-custportal"
+      cidr   = "135.136.130.0/26"
+      subnets = [
+        { name = "subnet1-1", cidr = "10.0.1.0/24" },
+        { name = "subnet1-2", cidr = "10.0.2.0/24" }
+      ]
+    },
+    
+    {
+      name   = "iad-vcn-pd-macc"
+      cidr   = "135.136.130.64/26"
+      subnets = [
+        { name = "subnet2-1", cidr = "10.1.1.0/24" },
+        { name = "subnet2-2", cidr = "10.1.2.0/24" }
+      ]
+    },
+    {
+      name   = "iad-vcn-pd-soup"
+      cidr   = "135.136.130.128/26"
+      subnets = [
+        { name = "subnet1-1", cidr = "10.0.1.0/24" },
+        { name = "subnet1-2", cidr = "10.0.2.0/24" }
+      ]
+    },
+    {
+      name   = "iad-vcn-pd-soup"
+      cidr   = "135.136.130.0/26"
+      subnets = [
+        { name = "subnet1-1", cidr = "10.0.1.0/24" },
+        { name = "subnet1-2", cidr = "10.0.2.0/24" }
+      ]
+    },
+  ]
+}
