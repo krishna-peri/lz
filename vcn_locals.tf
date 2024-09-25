5795afb062e1fcce191be0ea9b7bf930c3986afa
krishna-peri <krishna.peri@centroid.com>
2024-09-25 16:32:35 -0400

    Update vcn_locals.tf

--- vcn_locals.tf
+++ vcn_locals.tf
@@ -1,9 +1,9 @@
 locals {
-
-  user_ocid          = ocid1.user.oc1..aaaaaaaar5kjd23bf23pfa2o2u3x7fnxhxhlielljk3f3ql2cxqj545g3coa
-  fingerprint        = b6:a0:fc:23:3c:45:53:f7:90:7f:53:df:b7:7d:85:0a	
-  private_key_path   = kp.pem
-  region             = us-ashburn-1
+  tenancy_ocid       = "ocid1.tenancy.oc1..aaaaaaaacr2kimui4s25ck7zeyjrqvfc3bpyjlr73hjrydgukzwcwqctbqra"
+  user_ocid          = "ocid1.user.oc1..aaaaaaaar5kjd23bf23pfa2o2u3x7fnxhxhlielljk3f3ql2cxqj545g3coa"
+  fingerprint        = "b6:a0:fc:23:3c:45:53:f7:90:7f:53:df:b7:7d:85:0a"
+  private_key_path   = "kp.pem"
+  region             = "us-ashburn-1"
   comp_ocid       =  "ocid1.compartment.oc1..aaaaaaaafgnmba62o5ubvvkxmjzvagaeefs7wp6xcquh36bapdhctmndcpaa"
 
   
