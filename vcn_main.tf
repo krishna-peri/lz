4f6bd3604ab633147b49dbae92714e3f0d4b84d1
krishna-peri <krishna.peri@centroid.com>
2024-09-25 19:24:15 -0400

    Update vcn_main.tf

--- vcn_main.tf
+++ vcn_main.tf
@@ -24,13 +24,11 @@ resource "oci_core_vcn" "vcn" {
 
 # Loop to create Subnets
 resource "oci_core_subnet" "subnet" {
-  for_each = toset {for vcn in local.vcn_configs :vcn.name => {for subnet in vcn.subnets :subnet.name => subnet } }
+  for_each = { for vcn in local.vcn_configs :vcn.name => {for subnet in vcn.subnets :subnet.name => subnet } }
 
   ###count = length(local.vcn_configs)
   display_name = each.value[0].name
-  #display_name = each.value.subnet.name
   cidr_block   = each.value[0].cidr
-  #cidr_block   = each.value.subnet.cidr
   vcn_id       = oci_core_vcn.vcn[each.key].id
   compartment_id = var.tenancy_ocid  # Update to your compartment OCID
 
