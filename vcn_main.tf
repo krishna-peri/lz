0acf2ae90624b769cb0a426f88ca30c7ccd95111
krishna-peri <krishna.peri@centroid.com>
2024-09-26 01:34:50 -0400

    Update vcn_main.tf

--- vcn_main.tf
+++ vcn_main.tf
@@ -9,7 +9,7 @@ resource "oci_core_subnet" "subnets" {
   for_each = { for idx, subnet in local.subnet_details : idx => subnet }
   cidr_block     = each.value.cidr_block
   vcn_id         = oci_core_vcn[each.value.vcn_key].id
-  compartment_id = var.compartment_id
+  compartment_id = local.tenancy_ocid
   display_name   = each.value.display_name
   security_list_ids = [oci_core_security_list.security_lists[each.value.vcn_key].id]
   route_table_id    = oci_core_route_table.route_tables[each.value.vcn_key].id
