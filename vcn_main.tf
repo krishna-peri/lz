e66ce0bc08622d9c599770d9eff66c2cd0a95c65
krishna-peri <krishna.peri@centroid.com>
2024-09-26 10:53:17 -0400

    Update vcn_main.tf

--- vcn_main.tf
+++ vcn_main.tf
@@ -7,7 +7,7 @@ foreach = { for v in var.vcns : v.name => v }
 }
 
 resource "oci_core_subnet" "subnets" {
-  for_each = { for v in var.vcns, s in v.subnets : "${v.name}-${s.name}" => s }
+  for_each = { for v in var.vcns, s in v.subnets : "${v.name}-${s.name}" => "s" }
   vcn_id     = oci_core_vcn.vcn[(each.key)[0]].id
   cidr_block     = each.value.cidr_block
   compartment_id = local.tenancy_ocid
