c9f95a44e1e37f08b0cdcbb8754f3705fb08e2c3
krishna-peri <krishna.peri@centroid.com>
2024-09-25 22:46:35 -0400

    Update vcn_main.tf

--- vcn_main.tf
+++ vcn_main.tf
@@ -24,28 +24,28 @@ resource "oci_core_vcn" "vcn" {
 
 
 # Loop to create Subnets
-resource "oci_core_subnet" "subnet" {
-  for_each = { for vcn in local.vcn_configs :vcn.name => {for subnet in vcn.subnets :subnet.name => subnet } }
+###resource "oci_core_subnet" "subnet" {
+  ###for_each = { for vcn in local.vcn_configs :vcn.name => {for subnet in vcn.subnets :subnet.name => subnet } }
+  subnet_list= for vcn in local.vcn_configs :vcn.name => {for subnet in vcn.subnets :subnet.name => subnet }
 
   ###count = length(local.vcn_configs)
-  display_name = each.value[0].name
-  cidr_block   = each.value[0].cidr
-  vcn_id       = oci_core_vcn.vcn[each.key].id
-  compartment_id = var.tenancy_ocid  # Update to your compartment OCID
+  ####display_name = each.value[0].name
+  #####cidr_block   = each.value[0].cidr
+  ###vcn_id       = oci_core_vcn.vcn[each.key].id
+  ####compartment_id = var.tenancy_ocid  # Update to your compartment OCID
 
 
-  }
+  ##}
 
 output "vcn_ids" {
-  value = oci_core_vcn.vcn.*.id
+  value = oci_core_vcn.vcn.[*].id
 }
 
-output "subnet_ids" {
-  value = oci_core_subnet.subnet.*.id
+##output "subnet_ids" {
+  ##value = oci_core_subnet.subnet.*.id
+##}
+
+output "list" {
+  value = subnet_list
 }
 
-
-output "subnets_name" {
-value = { for vcn in local.vcn_configs :vcn.name => {for subnet in vcn.subnets :subnet.name => subnet }
- }
-}
