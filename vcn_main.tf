03f0393108487de4c35c30d72a710f52327431f1
krishna-peri <krishna.peri@centroid.com>
2024-09-26 01:17:47 -0400

    Update vcn_main.tf

--- vcn_main.tf
+++ vcn_main.tf
@@ -1,5 +1,5 @@
 resource "oci_core_vcn" "vcns" {
-  for_each = var.vcns
+  for_each       = local.vcns
   cidr_block     = each.value.cidr_block
   compartment_id = var.compartment_id
   display_name   = each.key
