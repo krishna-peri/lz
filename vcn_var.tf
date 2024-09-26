0b31ae6aa03040ce11c6d4e43d1343637b20302a
krishna-peri <krishna.peri@centroid.com>
2024-09-26 11:35:04 -0400

    Update vcn_var.tf

--- vcn_var.tf
+++ vcn_var.tf
@@ -4,26 +4,3 @@ variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
 variable "region" {}
-
-variable vcns  {
-description = "List of VCN"
-type        = list(map(any))
-default = [
-      name  = "iad-vcn-dmz" 
-      cidr_block = "135.136.129.0/26"
-      vcn_label = "iaddmz"
-      subnets = [
-        {
-        name = "iad-sn-np-openam-lb"
-        cidr_block   = "135.136.129.0/29"
-        is_public = false
-        }
-          
-      {
-        name = "Subnet2_VCN1"          
-        cidr_block   = "135.136.129.8/29"
-        is_public = true  
-      }
-      ]
-    ]
-  }
