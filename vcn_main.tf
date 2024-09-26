5aaaad6205046546407305f748e8dbb15c5b5b21
krishna-peri <krishna.peri@centroid.com>
2024-09-26 10:32:40 -0400

    Update vcn_main.tf

--- vcn_main.tf
+++ vcn_main.tf
@@ -2,7 +2,7 @@ resource "oci_core_vcn" "vcn" {
 for_each = { for v in var.vcns : v.name => v }
   cidr_block     = each.value.cidr_block
   compartment_id = local.tenancy_ocid
-  display_name   = each.key
+  display_name   = each.value.name
   dns_label      = each.value.vcn_label
 }
 
@@ -10,7 +10,6 @@ resource "oci_core_subnet" "subnets" {
   for_each = { for v in var.vcns, s in v.subnets : "${v.name}-${s.name}" => s }
   vcn_id     = oci_core_vcn.vcn[split("-", each.key)[0]].id
   cidr_block     = each.value.cidr_block
-  vcn_id         = oci_core_vcn[each.value.vcn_key].id
   compartment_id = local.tenancy_ocid
   display_name   = each.value.display_name
   prohibit_public_ip_on_vnic = each.value.type
