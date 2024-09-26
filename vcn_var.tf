7affcef30e64106edf02c6eb68ec376254808347
krishna-peri <krishna.peri@centroid.com>
2024-09-26 01:33:03 -0400

    Update vcn_var.tf

--- vcn_var.tf
+++ vcn_var.tf
@@ -4,14 +4,4 @@ variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
 variable "region" {}
-variable "vcns" {
-  type = map(object({
-    cidr_block = string
-    subnets = map(object({
-      cidr_block = string
-      display_name = string
-      type = string # "public" or "private"
-    }))
-  }))
-} = local.vcns
 
